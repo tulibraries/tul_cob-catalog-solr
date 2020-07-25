06ba56a43dc8dfdb2928e26517a9210086de7d21
David Kinzer <dtkinzer@gmail.com>
2020-07-25 11:17:25 -0400

    Run search relevancy tests.
    * Ports relevancy tests to non rails version.
    * Add docker-compose setup to run relevancy tests.
    * Adds Makefile for convenience
    * Updates circle ci to use make test.

---
+++ spec/relevance/searches_spec.rb
@@ -0,0 +1,103 @@
+# frozen_string_literal: true
+
+RSpec.describe "Searches" do
+  solr = RSolr.connect(url: ENV["SOLR_URL"])
+
+  let(:search_term) { search_terms.values.join(" ") }
+  let(:per_page) { 1 }
+
+  let(:response) { solr.get("search", params: { q: search_term, rows: per_page }) }
+  let(:docs) { response.dig("response", "docs") || [] }
+
+  context "search for Google" do
+    let(:search_term) { "Google" }
+
+    it "returns a documetn with Google in it's title" do
+      title = docs.first["title_statement_display"].join
+      expect(title).to eq("Google / Virginia Scott.")
+    end
+  end
+
+  context "search an ID" do
+    let(:search_term)  { "991032411489703811" }
+
+    it "returns a document with that id" do
+      id = docs.first["id"]
+      expect(id).to eq("991032411489703811")
+    end
+  end
+
+
+  context "search creator" do
+    let(:search_term) { "Scott, Virginia A." }
+
+    it "returns a document for that creator" do
+      title = docs.first["title_statement_display"].join
+      expect(title).to eq("Google / Virginia Scott.")
+    end
+  end
+
+  context "search imprint" do
+    let(:search_term) { "Westport, Conn. : Greenwood Press, 2008."
+     }
+
+    it "returns a document for that imprint" do
+      title = docs.first["title_statement_display"].join
+      expect(title).to eq("Google / Virginia Scott.")
+    end
+  end
+
+  context "search series title" do
+    let(:search_term) { "SupersonicBionicRobotVoodooPower" }
+
+    it "returns a document with that series title" do
+      title = docs.first["title_statement_display"].join
+      expect(title).to eq("Google / Virginia Scott.")
+    end
+  end
+
+  context "search content" do
+    let(:search_term) { "OctagonOxygenAluminumIntoxicants" }
+
+    it "returns a document with that specific content" do
+      title = docs.first["title_statement_display"].join
+      expect(title).to eq("Google / Virginia Scott.")
+    end
+  end
+
+  context "search subject" do
+    let(:search_term) { "ParamedicsFedExYourLegsWithEggs" }
+
+    it "returns a document with that specific subject" do
+      title = docs.first["title_statement_display"].join
+      expect(title).to eq("Google / Virginia Scott.")
+    end
+  end
+
+  context "search isbn" do
+    let(:search_term) { "9780313351273" }
+
+    it "returns a document with that specific isbn" do
+      title = docs.first["title_statement_display"].join
+      expect(title).to eq("Google / Virginia Scott.")
+    end
+  end
+
+  context "search lccn" do
+    let(:search_term) { "2008030541" }
+
+    it "returns a document with that specific lccn" do
+      title = docs.first["title_statement_display"].join
+      expect(title).to eq("Google / Virginia Scott.")
+    end
+  end
+
+  context "search term as colon" do
+    let(:search_term) { "Religious liberty :the positive dimension : an address" }
+
+    it "returns a document with that specific lccn" do
+      title = docs.first["title_statement_display"].join
+      expect(title).to eq("Religious liberty : the positive dimension : an address / by Franklin H. Littell at Doane College on April 26, 1966.")
+    end
+  end
+end
