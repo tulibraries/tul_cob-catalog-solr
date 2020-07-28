2da51c484a2ad7d2ec668e8e45cad93c6551e58f
David Kinzer <dtkinzer@gmail.com>
2020-07-28 15:43:51 -0400

    Run search relevancy tests. (#19)
    * Run search relevancy tests.
    
    * Ports relevancy tests to non rails version.
    * Add docker-compose setup to run relevancy tests.
    * Adds Makefile for convenience
    * Updates circle ci to use make test.
    
    * setup remote docker
    
    * Use official ruby image.
    
    * use app dir.
    
    * Need to switch to machine because CI doesn't allow compose voluming mounting.
    
    * set-up-remote-docker is not a machine executor.
    
    * Add gem caching.
    
    * Wait until solr is up before loading.
    
    * Fix script.
    
    * Remove unused scripts.
    
    * Do not package all the things.
    
    * Update README.
    
    * Rename make command.

---
+++ Gemfile
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
+
+gem "rsolr"
+gem "rspec"
+gem "rails"
+gem "cob_index",
+  git: "https://github.com/tulibraries/cob_index.git",
+  branch: "fix-broke-arg-name"
+  #branch: "main"
+gem "alma",
+  git: "https://github.com/tulibraries/alma_rb.git",
+  branch: "main"
---
+++ spec/relevance/tul_specific_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+RSpec.describe "some faceted queries." do
+  solr = RSolr.connect(url: ENV["SOLR_URL"])
+  let(:docs) { response.dig("response", "docs") || [] }
+  let(:ids) { docs.map { |doc| doc.fetch("id") }.compact }
+
+  context "a faceted search for Presser Listening Library " do
+    let(:response) { solr.get("search", params: {
+      fq: ["{!term f=library_facet}Presser Listening Library"],
+      "rows": 50,
+    })}
+
+    it "has results with another library before before results only at presser" do
+      expect(ids)
+        .to include_items(%w[991025170559703811 991012972279703811 991036165169703811])
+        .before(["991034751269703811"])
+    end
+  end
+end
