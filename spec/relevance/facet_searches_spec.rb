afc0d1705a176b75cee2ce229ec0ff39a5f41c74
sensei100 <tuh42082@temple.edu>
2024-12-12 16:26:49 -0500

    Remove pry

--- spec/relevance/facet_searches_spec.rb
+++ spec/relevance/facet_searches_spec.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 require "spec_helper"
-require "pry"
 
 RSpec.describe "Facet searches" do
   solr = RSolr.connect(url: ENV["SOLR_URL"])
@@ -14,7 +13,6 @@ RSpec.describe "Facet searches" do
 
   context "facet search results are case insensitive" do
     it "returns the same number of results" do
-      binding.pry
       expect(response_uppercase['response']['numFound']).to eq(response_lowercase['response']['numFound'])    
     end
   end
