48f0692c34458a2f16baf679fa85cf8daf990825
David Kinzer <dtkinzer@gmail.com>
2024-12-12 09:27:38 -0500

    BL-1855: Add test to check that quoted results all contain exact term.

--- spec/relevance/quoted_searches_spec.rb
+++ spec/relevance/quoted_searches_spec.rb
@@ -2,31 +2,28 @@
 require "spec_helper"
 
 RSpec.describe "Searches with quotes in the terms" do
-  before do
-  end
-
     let(:solr)  { RSolr.connect(url: ENV["SOLR_URL"]) }
     let(:term) { "" }
     let(:quoted_term) { "\"#{term}\""}
     let(:solr_path) { "search" }
     let(:extra_params) { {} }
-    let(:num_found_quoted) {
-      results = solr.get(solr_path, params: { q: quoted_term  }
+    let(:quoted_results) {
+      solr.get(solr_path, params: { q: quoted_term  }
         .merge(extra_params))
-      results["response"]["numFound"]
     }
-    let(:num_found_not_quoted) {
-      results = solr.get("search", params: { q: term  }
+    let(:non_quoted_results) {
+      solr.get("search", params: { q: term  }
         .merge(extra_params))
-      results["response"]["numFound"]
     }
+    let(:count_quoted_results) { quoted_results["response"]["numFound"] }
+    let(:count_not_quoted_results) { non_quoted_results["response"]["numFound"] }
 
 
   context "quoted queries with more than one term" do
     let(:term) { "book readers"}
 
     it "returns less results when quoted" do
-      expect(num_found_quoted).to be < num_found_not_quoted
+      expect(count_quoted_results).to be < count_not_quoted_results
     end
   end
 
@@ -36,19 +33,29 @@ RSpec.describe "Searches with quotes in the terms" do
     let(:solr_path) { "single_quoted_search" }
 
     it "returns less results when quoted" do
-      expect(num_found_quoted).to be < num_found_not_quoted
+      expect(count_quoted_results).to be < count_not_quoted_results
     end
   end
 
 
   context "title quoted query with one term" do
     let(:term) { "readers" }
-    let(:extra_params) { { qf: "${title_qf}", pf: "${title_pf}, fl: " }}
+    let(:extra_params) {
+      { qf: "${title_qf}", pf: "${title_pf}",
+        fl: "marc_display_raw", rows: 49 }
+    }
     # In the application with override this path dynamically.
     let(:solr_path) { "single_quoted_search" }
 
     it "returns less results when quoted" do
-      expect(num_found_quoted).to be < num_found_not_quoted
+      expect(count_quoted_results).to be < count_not_quoted_results
+    end
+
+    it "only returns results that contain the searched term" do
+      all_results_contain_search_term = quoted_results["response"]["docs"].all? do |doc|
+        doc["marc_display_raw"]&.match /#{term }/i
+      end
+      expect(all_results_contain_search_term)
     end
   end
 
@@ -57,7 +64,7 @@ RSpec.describe "Searches with quotes in the terms" do
     let(:extra_params) { { qf: "${title_qf}", pf: "${title_pf}" }}
 
     it "returns less results when quoted" do
-      expect(num_found_quoted).to be < num_found_not_quoted
+      expect(count_quoted_results).to be < count_not_quoted_results
     end
   end
 
@@ -68,7 +75,7 @@ RSpec.describe "Searches with quotes in the terms" do
     let(:solr_path) { "single_quoted_search" }
 
     it "returns less results when quoted" do
-      expect(num_found_quoted).to be < num_found_not_quoted
+      expect(count_quoted_results).to be < count_not_quoted_results
     end
   end
 
@@ -77,7 +84,7 @@ RSpec.describe "Searches with quotes in the terms" do
     let(:extra_params) { { qf: "${subject_qf}", pf: "${subject_pf}" }}
 
     it "returns less results when quoted" do
-      expect(num_found_quoted).to be < num_found_not_quoted
+      expect(count_quoted_results).to be < count_not_quoted_results
     end
   end
 
@@ -88,7 +95,7 @@ RSpec.describe "Searches with quotes in the terms" do
     let(:extra_params) { { qf: "${author_qf}", pf: "${author_pf}" }}
 
     it "returns less results when quoted" do
-      expect(num_found_quoted).to be < num_found_not_quoted
+      expect(count_quoted_results).to be < count_not_quoted_results
     end
   end
 
@@ -97,7 +104,7 @@ RSpec.describe "Searches with quotes in the terms" do
     let(:extra_params) { { qf: "${author_qf}", pf: "${author_pf}" }}
 
     it "returns less results when quoted" do
-      expect(num_found_quoted).to be < num_found_not_quoted
+      expect(count_quoted_results).to be < count_not_quoted_results
     end
   end
 
